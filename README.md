343ce06a40aa3dd33b2ad9c75d47357aa47353f0
Steve Engledow <steve@offend.me.uk>
2018-03-26 14:48:55 +0100

    Add some lovely features and a readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# PS1
+
+My custom PS1 prompt.
+
+Includes:
+* Currently selected AWS profile
+* Git branch if in a git repo directory
+* Username, hostname and current path
--- ps1.sh
+++ ps1.sh
@@ -1,14 +1,48 @@
-# Wonderful git branch function
-get_git_branch() {
-	gitbranch=$(git branch 2>/dev/null | grep "*" | awk '{print $2}')
-	if [ -n "$gitbranch" ]; then
-		gitbranch="$gitbranch\n"
-	fi
-	echo $gitbranch
+# Laziness is key
+colourise() {
+    echo -e "\033[$1m$2\033[0m"
 }
 
-PS1+="$(get_git_branch)"
-PS1='\[\033[0;37m\]\u@\h:\w\[\033[0m\]\n'
-PS1+="> "
+git_branch() {
+	gitbranch=$(git branch 2>/dev/null | grep "*" | awk '{print $2}')
+	if [ -n "$gitbranch" ]; then
+        echo git: $gitbranch
+	fi
+}
 
-export PS1
+aws_profile() {
+    if [ -n "$AWS_PROFILE" ]; then
+        echo aws: $AWS_PROFILE
+    fi
+}
+
+do_plugins() {
+    prompt=""
+    for plugin in ${!ps1_plugins[@]}; do
+        colour=${ps1_plugins[$plugin]}
+        output=$(eval $plugin)
+
+        if [ -n "$output" ]; then
+            prompt+=$(colourise "$colour" "$output")
+            prompt+="\n"
+        fi
+    done
+
+    if [ -n "$prompt" ]; then
+        echo -e "$prompt\033"
+    fi
+}
+
+prompt() {
+    echo -n '$(do_plugins)'
+    colourise "1;37" "\u@\h:\w"
+    echo "> "
+}
+
+if [ -z ${ps1_plugins+x} ]; then
+    declare -A ps1_plugins=()
+    ps1_plugins[git_branch]="36"
+    ps1_plugins[aws_profile]="33"
+fi
+
+export PS1=$(prompt)
